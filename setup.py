b75ae8891e10969f647e35b086a21ad4682fa870
Jay Kanakia <jay.kanakia26@gmail.com>
2026-03-31 19:02:17 +0530

    initial data cleaning

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A end to end Project on swiggy delivery time prediction using mlops principle and deploying on AWS',
+    author='Jay Kanakia',
+    license='',
+)
